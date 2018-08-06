b95433e0ce4135302a28cf1346d02dbc34339d07
liulu <liulu@ifreecomm.com>
2018-08-06 09:18:40 +0800

    实现添加页面3

---
+++ src/components/public.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="father">
+      father content
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "public"
+    }
+</script>
+
+<style scoped>
+
+</style>
